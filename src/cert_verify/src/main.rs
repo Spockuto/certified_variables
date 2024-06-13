eb1250e120f283ae39469ba54f103d10d6d7be59
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2024-06-13 15:34:40 +0000

    Cleanup code

--- src/cert_verify/src/main.rs
+++ src/cert_verify/src/main.rs
@@ -10,11 +10,10 @@ use ic_certificate_verification::VerifyCertificate;
 use ic_certification::hash_tree::HashTree;
 use ic_certification::{Certificate, LookupResult};
 use rand::prelude::*;
-use serde::Serialize;
 use serde_cbor::Deserializer;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Arbitrary)]
+#[derive(CandidType, Deserialize, Debug, PartialEq, Eq, Arbitrary)]
 struct User {
     name: String,
     age: u8,
@@ -27,9 +26,10 @@ struct CertifiedUser {
     witness: Vec<u8>,
 }
 
-static URL: &str = "http://localhost:35473";
+static URL: &str = "http://localhost:40073";
 static CANISTER: &str = "a3shf-5eaaa-aaaaa-qaafa-cai";
 const MAX_CERT_TIME_OFFSET_NS: u128 = 300_000_000_000; // 5 min
+const MAX_CALLS: usize = 10;
 
 #[tokio::main]
 async fn main() {
@@ -41,7 +41,8 @@ async fn main() {
         .build()
         .expect("Unable to create agent");
 
-    // Only for demo purposes. Hard code when using for IC
+    // This should be done only in demo environments.
+    // When interacting with mainnet, hardcode the root_key.
     agent
         .fetch_root_key()
         .await
@@ -50,26 +51,42 @@ async fn main() {
 
     let canister_id = Principal::from_text(CANISTER).unwrap();
 
-    // Make 5 random update calls for set_user
-    let mut inc_calls = Vec::new();
-    for i in 0..5 {
+    // ==== START of canister data setup
+    // Make MAX_CALLS to set_user
+    let mut get_user_calls = Vec::new();
+    for _ in 0..MAX_CALLS {
         let bytes: [u8; 16] = rng.gen();
         let mut u = Unstructured::new(&bytes[..]);
         let temp_user = User::arbitrary(&mut u).unwrap();
 
-        println!("Calling set_user at {:?} with {:?}", i, temp_user);
+        println!("Calling set_user with {:?}", temp_user);
         let response = agent
             .update(&canister_id, "set_user")
             .with_effective_canister_id(canister_id)
             .with_arg(Encode!(&temp_user).unwrap())
             .call_and_wait();
-        inc_calls.push(response);
+        get_user_calls.push(response);
     }
-    join_all(inc_calls).await;
+    let results: Vec<u64> = join_all(get_user_calls)
+        .await
+        .into_iter()
+        .map(|result| {
+            Decode!(
+                result
+                    .expect("Unable to call query call set_user")
+                    .as_slice(),
+                u64
+            )
+            .unwrap()
+        })
+        .collect();
 
-    // call a random index with get_user
-    let index: u64 = rng.gen();
-    let index: u64 = index % 5 + 1;
+    // From response indexes, chose a random index for get_user
+    let index: usize = rng.gen();
+    let index: u64 = *results.get(index % MAX_CALLS).unwrap();
+    // ==== END of canister data setup
+
+    println!("Fetching index {:?}", index);
 
     let query_response = agent
         .query(&canister_id, "get_user")
@@ -78,9 +95,10 @@ async fn main() {
         .call()
         .await
         .expect("Unable to call query call get_user");
-    let cert_decoded = Decode!(&query_response, CertifiedUser).unwrap();
 
-    let mut deserializer = Deserializer::from_slice(&cert_decoded.certificate);
+    let certified_user = Decode!(&query_response, CertifiedUser).unwrap();
+
+    let mut deserializer = Deserializer::from_slice(&certified_user.certificate);
     let certificate: Certificate = serde::de::Deserialize::deserialize(&mut deserializer).unwrap();
 
     let start = SystemTime::now();
@@ -89,50 +107,55 @@ async fn main() {
         .expect("Time went backwards")
         .as_nanos();
 
-    // Digest equality and signature verification are done here
+    // Step 1: Check if signature in the certificate matches
+    // root_hash of the tree in certificate as message and root_key as public_key
     let verification_result = certificate.verify(canister_id.as_slice(), &root_key[..]);
-    // Time skew is verified here
+
+    println!(
+        "Step 1: Digest match & Signature verification: {:?}",
+        verification_result
+    );
+
+    // Step 2: Check if the response is not stale with the given time offset MAX_CERT_TIME_OFFSET_NS.
     let time_verification_result =
         validate_certificate_time(&certificate, &current_time, &MAX_CERT_TIME_OFFSET_NS);
 
-    println!(
-        "Verification result (Digest match & Signature verification): {:?}",
-        verification_result
-    );
-    println!("Time skew: {:?}", time_verification_result);
+    println!("Step 2: Time skew: {:?}", time_verification_result);
 
-    // Check if witness is in the tree
-    let lookup_result = certificate.tree.lookup_path([
-        "canister".as_bytes(),
-        canister_id.as_slice(),
-        "certified_data".as_bytes(),
-    ]);
+    // Step 3: Check if witness root_hash matches the certified_data
+    let lookup_result =
+        certificate
+            .tree
+            .lookup_path([b"canister", canister_id.as_slice(), b"certified_data"]);
 
-    let mut deserializer = Deserializer::from_slice(&cert_decoded.witness);
-    let witness_decoded: HashTree<Vec<u8>> =
-        serde::de::Deserialize::deserialize(&mut deserializer).unwrap();
-
-    let witness_digest = witness_decoded.digest();
-
-    let cert_var: [u8; 32] = match lookup_result {
+    let certified_data: [u8; 32] = match lookup_result {
         LookupResult::Found(result) => result.try_into().unwrap(),
         _ => panic!("Certified data not found"),
     };
 
+    let mut deserializer = Deserializer::from_slice(&certified_user.witness);
+    let witness_decoded: HashTree<Vec<u8>> =
+        serde::de::Deserialize::deserialize(&mut deserializer).unwrap();
+    let witness_digest = witness_decoded.digest();
+
     println!(
-        "Witness root_hash mataches certified data: {:?} ",
-        witness_digest == cert_var
+        "Step 3: Witness digest mataches certified data: {:?} ",
+        witness_digest == certified_data
     );
 
+    // Step 4: Check if the query parameters are in the witness
     let witness_lookup: User =
         match witness_decoded.lookup_path([b"user", &index.to_be_bytes()[..]]) {
             LookupResult::Found(result) => serde_cbor::from_slice(result).unwrap(),
             _ => panic!("user {} not found", index),
         };
 
+    // Step 5: Check if the data found in Witness matches the returned result from the query.
     println!(
-        "Witness data matches User value: {:?}",
-        witness_lookup == cert_decoded.user
+        "Step 4 & Step 5: Witness data matches User value: {:?}",
+        witness_lookup == certified_user.user
     );
-    println!("Result: {:?}", cert_decoded.user);
+
+    // Step 6: Return the result
+    println!("Result: {:?}", certified_user.user);
 }
